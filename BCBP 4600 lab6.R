b12b551b4c358cfce384c486f5973711dc9d0403
JennyHuan <144462555+JennyHuan@users.noreply.github.com>
2023-11-28 11:36:42 -0500

    BCBP 4600 lab6

---
+++ BCBP 4600 lab6.R
@@ -0,0 +1,77 @@
+library(gcookbook)
+ggplot(BOD, aes(x=Time, y=demand)) + geom_line()
+BOD
+BOD1 <- BOD # make a copy of the dataset
+BOD1$Time <- factor(BOD1$Time)
+library(ggplot2)
+ggplot(BOD1, aes(x=Time, y=demand, group=1)) + geom_line()
+ggplot(BOD, aes(x=Time, y= demand)) +geom_line() + ylim(0, max(BOD$demand))
+ggplot(BOD, aes(x=Time, y=demand)) +geom_line() + expand_limits(y=0)
+# Adding points to a line graph
+ggplot(BOD, aes(x=Time, y=demand)) + geom_line() + geom_point()
+library(gcookbook) # For the data set
+ggplot(worldpop, aes(x=Year, y=Population)) + geom_line() + geom_point()
+# same with log-y axis
+ggplot(worldpop, aes(x=Year, y=Population)) + geom_line() + geom_point() + scale_y_log10()
+
+# lab3_rpart1
+require(rpart)
+Swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+plot(Swiss_rpart) # try some different plot options
+text(Swiss_rpart) # try some different text options
+
+# lab3_rpart2
+# Regression Tree Example
+require(rpart)
+# build the  tree
+fitM <- rpart(Mileage~Price + Country + Reliability + Type, method="anova", data=cu.summary)
+printcp(fitM) # display the results
+plotcp(fitM)
+summary(fitM)
+par(mfrow=c(1,2)) 
+rsq.rpart(fitM) # visualize cross-validation results
+# plot tree
+plot(fitM, uniform=TRUE, main="Regression Tree for Mileage ")
+text(fitM, use.n=TRUE, all=TRUE, cex=.8)
+# prune the tree
+pfitM<- prune(fitM, cp=0.01160389) # from cptable??? adjust this to see the effect
+# plot the pruned tree
+plot(pfitM, uniform=TRUE, main="Pruned Regression Tree for Mileage")
+text(pfitM, use.n=TRUE, all=TRUE, cex=.8)
+post(pfitM, file = "ptree2.ps", title = "Pruned Regression Tree for Mileage")
+
+# lab3_rpart3
+library(e1071)
+library(rpart)
+data(Glass, package="mlbench")
+index <- 1:nrow(Glass)
+testindex <- sample(index, trunc(length(index)/3))
+testset <- Glass[testindex,]
+trainset <- Glass[-testindex,]
+rpart.model <- rpart(Type ~ ., data = trainset)
+rpart.pred <- predict(rpart.model, testset[,-10], type = "class")
+printcp(rpart.model)
+plotcp(rpart.model)
+
+rsq.rpart(rpart.model)
+print(rpart.model)
+
+plot(rpart.model,compress=TRUE)
+text(rpart.model, use.n=TRUE)
+plot(rpart.pred)
+
+# lab3_rpart4
+fitK <- rpart(Kyphosis ~ Age + Number + Start, method="class", data=kyphosis)
+printcp(fitK) # display the results
+plotcp(fitK) # visualize cross-validation results
+summary(fitK) # detailed summary of splits
+# plot tree
+plot(fitK, uniform=TRUE, main="Classification Tree for Kyphosis")
+text(fitK, use.n=TRUE, all=TRUE, cex=.8)
+# create attractive postscript plot of tree
+post(fitK, file = "kyphosistree.ps", title = "Classification Tree for Kyphosis") # might need to convert to PDF (distill)
+
+pfitK <- prune(fitK, cp = fitK$cptable[which.min(fitK$cptable[,"xerror"]),"CP"])
+plot(pfitK, uniform=TRUE, main="Pruned Classification Tree for Kyphosis")
+
+
